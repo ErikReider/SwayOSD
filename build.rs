42037f9768eba1126f832cd9dcd0baaf3def0e9c
Erik Reider <35975961+ErikReider@users.noreply.github.com>
2023-12-08 13:40:56 +0100

    Create Action rust-format.yml

--- build.rs
+++ build.rs
@@ -2,10 +2,7 @@ use std::{env, process::Command};
 
 fn main() {
 	let output = Command::new("glib-compile-resources")
-		.args(&[
-			"./data/swayosd.gresource.xml",
-			"--sourcedir=./data",
-		])
+		.args(&["./data/swayosd.gresource.xml", "--sourcedir=./data"])
 		.arg(&format!(
 			"--target={}/swayosd.gresource",
 			env::var("OUT_DIR").unwrap()
