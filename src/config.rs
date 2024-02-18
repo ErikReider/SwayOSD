d02d9d8c01a192e27260f5021f37c712450ee4ab
Ferdinand Bachmann <ferdinand.bachmann@yrlf.at>
2024-02-18 14:38:54 +0100

    Add config file format and config file parsing

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,10 @@
 #![allow(dead_code)]
 
+#[path = "config/backend.rs"]
+pub mod backend;
+#[path = "config/user.rs"]
+pub mod user;
+
 pub const DBUS_PATH: &str = "/org/erikreider/swayosd";
 pub const DBUS_BACKEND_NAME: &str = "org.erikreider.swayosd";
 pub const DBUS_SERVER_NAME: &str = "org.erikreider.swayosd-server";
---
+++ src/config/user.rs
@@ -0,0 +1,53 @@
+use gtk::glib::system_config_dirs;
+use gtk::glib::user_config_dir;
+use serde_derive::Deserialize;
+use std::error::Error;
+use std::path::PathBuf;
+
+#[derive(Deserialize, Default, Debug)]
+#[serde(deny_unknown_fields)]
+pub struct ClientConfig {}
+
+#[derive(Deserialize, Default, Debug)]
+#[serde(deny_unknown_fields)]
+pub struct ServerConfig {
+	pub style: Option<PathBuf>,
+	pub top_margin: Option<f32>,
+	pub max_volume: Option<u8>,
+}
+
+#[derive(Deserialize, Default, Debug)]
+#[serde(deny_unknown_fields)]
+pub struct UserConfig {
+	#[serde(default)]
+	pub server: ServerConfig,
+	#[serde(default)]
+	pub client: ClientConfig,
+}
+
+fn find_user_config() -> Option<PathBuf> {
+	let path = user_config_dir().join("swayosd").join("config.toml");
+	if path.exists() {
+		return Some(path);
+	}
+
+	for path in system_config_dirs() {
+		let path = path.join("swayosd").join("config.toml");
+		if path.exists() {
+			return Some(path);
+		}
+	}
+
+	None
+}
+
+pub fn read_user_config() -> Result<UserConfig, Box<dyn Error>> {
+	let path = match find_user_config() {
+		Some(path) => path,
+		None => return Ok(Default::default()),
+	};
+
+	let config_file = std::fs::read_to_string(path)?;
+	let config: UserConfig = toml::from_str(&config_file)?;
+	Ok(config)
+}
