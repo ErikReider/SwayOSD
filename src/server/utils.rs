19526af51ec9049a86d188e72f4dc205aaf977f4
Vizkid04 <chesskid04@gmail.com>
2025-06-05 17:48:22 +0530

    fix : --device flag not working has been fixed.

--- src/server/utils.rs
+++ src/server/utils.rs
@@ -213,18 +213,17 @@ pub fn change_device_volume(
 		VolumeDeviceType::Sink(controller) => {
 			let server_info = controller.get_server_info();
 			let global_name = get_device_name();
-			let device_name: String = if global_name.is_none() {
-				match server_info {
-					Ok(info) => info.default_sink_name.unwrap_or("".to_string()),
-					Err(e) => {
-						eprintln!("Error getting default_sink: {}", e);
-						return None;
-					}
-				}
-			} else {
-				set_device_name(DEVICE_NAME_DEFAULT.to_string());
-				get_device_name().unwrap()
-			};
+            let device_name: String = if let Some(name) = global_name { //correcting if-else bug
+                name
+            } else {
+                match server_info {
+                    Ok(info) => info.default_sink_name.unwrap_or("".to_string()),
+                    Err(e) => {
+                        eprintln!("Error getting default_sink: {}", e);
+                        return None;
+                    }
+                }
+            };
 			match controller.get_device_by_name(&device_name) {
 				Ok(device) => (device, device_name.clone()),
 				Err(_) => {
